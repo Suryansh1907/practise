cf19237121154fc9ed5df5945bda6abf1441b435
suryansh <suryansh@1907gmail.com>
2026-05-11 04:11:43 +0530

    2 node app

---
+++ 02Starting Project/src/components/HelpBox.jsx
@@ -0,0 +1,13 @@
+import './HelpBox.css';
+import React from "react";
+
+function HelpBox({ title, text }) {
+  return (
+    <article className="help-box">
+      <h2>{title}</h2>
+      <p>{text}</p>
+    </article>
+  );
+}
+
+export default HelpBox;
---
+++ 02Starting Project/src/components/MainContent.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react';
+import React from "react";
+
+import HelpArea from './HelpArea';
+
+function MainContent() {
+  const [helpVisible, setHelpVisible] = useState(false);
+
+  function toggleHelp() {
+    setHelpVisible((isVisible) => !isVisible);
+  }
+
+  return (
+    <main>
+      <button onClick={toggleHelp}>{helpVisible ? 'Hide' : 'Show'} Help</button>
+      {helpVisible && <HelpArea />}
+    </main>
+  );
+}
+
+export default MainContent;
